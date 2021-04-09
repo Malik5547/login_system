d1b491142e7713f73cac8ab595b4fd8de8d95553
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-04-09 21:23:24 +0300

    you can log in but can not log out

--- login/models.py
+++ login/models.py
@@ -6,4 +6,4 @@ class User(models.Model):
     password = models.CharField(max_length=50)
 
     def __str__(self):
-        print(username)
+        print(self.username)
--- login/views.py
+++ login/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import User
+
 
 def index(request):
-    return HttpResponse("Yo!")
+    user = request.session.get('user')
+    if user is None:
+        return login(request)
+    else:
+        context = {'user': user}
+        return render(request, 'login/index.html', context)
+
+
+def login(request):
+    if request.POST:
+        if is_login_valid(request):
+            set_login_session(request)
+            return render(request, 'login/success.html')
+        else:
+            return render(request, 'login/login.html', {'wrong_login': True})
+    else:
+        return render(request, 'login/login.html')
+
+
+def is_login_valid(request):
+    username_req = request.POST['username']
+    password_req = request.POST['password']
+
+    try:
+        user = User.objects.filter(
+            username=username_req,
+            password=password_req,
+        )
+    except (KeyError, User.DoesNotExist):
+        return False
+
+    return True
+
+
+def set_login_session(request):
+    request.session['user'] = request.POST['username']
